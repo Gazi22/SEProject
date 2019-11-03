f133218e2478a58c22c793afada997223758bb62
Visnuciry <56089135+Visnuciry@users.noreply.github.com>
2019-11-03 19:42:14 +0100

    css correction

--- PokerProject/src/poker/version_graphics/view/PokerGameView.java
+++ PokerProject/src/poker/version_graphics/view/PokerGameView.java
@@ -5,16 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import poker.version_graphics.PokerGame;
@@ -74,7 +67,7 @@ public class PokerGameView {
 		VBox v2 = new VBox(name1, name2, players2, name3, players3, name4, players4);
 
 		v1.getStyleClass().add("vbox");
-		v2.getStyleClass().add("vbox2");
+		
 		
 		v1.setAlignment(Pos.CENTER);
 		v2.setAlignment(Pos.CENTER);
