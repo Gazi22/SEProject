0acb8682594561dd565750b3ad01056c3ea5c98c
Gazmend Shefiu <gazmend_shefiu@hotmail.com>
2019-11-03 03:22:41 +0100

    Revert "Merge branch 'master' into Gazi_Update5"
    This reverts commit 198d86dd8f5c774ec7024def34d04e245cb9afdc.

--- PokerProject/src/poker/version_graphics/view/PokerGameView.java
+++ PokerProject/src/poker/version_graphics/view/PokerGameView.java
@@ -1,33 +1,14 @@
 package poker.version_graphics.view;
 
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-
-import javafx.scene.image.Image;
-
 import javafx.scene.control.TextField;
-
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import poker.version_graphics.PokerGame;
 import poker.version_graphics.model.Player;
@@ -49,8 +30,7 @@ public class PokerGameView {
 	public static TextField name4 = new TextField();
 	public static TextField[] arrayTextFields = { name1, name2, name3, name4 };
 
-	
-	public PokerGameView(Stage stage, PokerGameModel model) throws FileNotFoundException {
+	public PokerGameView(Stage stage, PokerGameModel model) {
 		this.stage = stage;
 		this.model = model;
 
@@ -76,39 +56,6 @@ public class PokerGameView {
 		Button players3 = new Button("Start with 3 Players!");
 		Button players4 = new Button("Start with 4 Players!");
 
-		Label lbs3 = new Label("How many players are you?");
-		final Region spacer1 = new Region();
-		Button players2 = new Button ("2 Players");
-		Button players3 = new Button ("3 Players");
-		Button players4 = new Button ("4 Players");
-		Button chng = new Button ("Start!");
-		
-		TextField name1 = new TextField();
-		TextField name2 = new TextField();
-		TextField name3 = new TextField();
-		TextField name4 = new TextField();
-		
-		name1.setPromptText("Please enter Name of Player 1");
-		name2.setPromptText("Please enter Name of Player 2");
-		name3.setPromptText("Please enter Name of Player 3");
-		name4.setPromptText("Please enter Name of Player 4");
-		
-		Player player1 = new Player(name1.getText());
-		Player player2 = new Player(name2.getText());
-		Player player3 = new Player(name3.getText());
-		Player player4 = new Player(name4.getText());
-
-		name1.setPrefSize(10, 10);
-		name2.setPrefSize(10, 10);
-		name3.setPrefSize(10, 10);
-		name4.setPrefSize(10, 10);
-		
-		lbs.setTextFill(Color.GOLD);
-		lbs2.setTextFill(Color.BLACK);
-		lbs3.setTextFill(Color.GOLD);
-		spacer1.setMinHeight(125);
-		
-		
 		BorderPane boot = new BorderPane();
 
 		VBox v1 = new VBox(lb, lbs, lbs2);
@@ -120,36 +67,9 @@ public class PokerGameView {
 		boot.setTop(v1);
 		boot.setCenter(v2);
 
-		
-
-		//boot.getStylesheets().add(
-             //   getClass().getResource("poker.css").toExternalForm());
-              
-
-		VBox v1 = new VBox(spacer1, lbs, lbs2, lbs3);
-		VBox v2 = new VBox(name1, name2, players2, name3, players3, name4, players4);
-		VBox v3 = new VBox(chng);
-
-        v1.getStyleClass().add("vboxscene"); // CSS style class
-        
-        //add to Pane
-        boot.setTop(spacer1);
-        boot.setCenter(h);
-        boot.setBottom(chng);
-        
-        
-        
-        //Set background
-        Image image1 = new Image(new FileInputStream("C:\\Users\\visnu\\git\\SEProject\\PokerProject\\src\\poker\\version_graphics\\view\\PokerStartScene.jpg"));
-        BackgroundSize bSize = new BackgroundSize (500,500,false,false,true,false);
-		boot.setBackground(new Background(new BackgroundImage(image1, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,bSize)));
-		
-
-
+		this.scene1 = new Scene(boot, 400, 400);
 		stage.setTitle("Poker");
-		
-		
-		
+
 		// Create the control area
 		controls = new ControlArea();
 		controls.linkDeck(model.getDeck()); // link DeckLabel to DeckOfCards in the logic
@@ -225,7 +145,5 @@ public class PokerGameView {
 	public Button getBackButton() {
 		return controls.btnBack;
 	}
-	
-	
-	
+
 }
